7e2d7067e4ef435d0ba63a89df5ac70d137d8ce8
Developer Mamun <mdalmamun5354@gmail.com>
2025-02-22 11:05:38 +0600

    project page running

---
+++ scripts/projects.js
@@ -0,0 +1,115 @@
+const TYPE_ANDROID =1, TYPE_WEB= 2;
+
+let projects = [
+    {
+        key: "am-play",
+        name: "AM Play",
+        description: "Music player with advance tagging system.",
+        duration: "Android Studio + Java",
+        repo: "https://github.com/mdalmamunDev/AM-Play",
+        link: "android-apps/AM Play - MIN_8.2024.02.26.apk",
+        type: TYPE_ANDROID,
+        image: "am-play.png",
+        about: true,
+    },
+    {
+        key: "skill-syntax",
+        name: "Online Course Management (SkillSyntax)",
+        description: "A complete course management system with course creation and enrollment.",
+        duration: "Collaborative Project",
+        repo: "https://github.com/mad-programmers3/skill-syntax",
+        link: "",
+        type: TYPE_WEB,
+        image: "skill-syntax.png"
+    },
+    {
+        key: "am-play-web",
+        name: "AM Play Web",
+        description: "Web based music player with advance tagging system.",
+        duration: "Vue.js",
+        repo: "https://github.com/mdalmamunDev/AM-Play-Web",
+        link: "https://mdalmamundev.github.io/AM-Play-Web",
+        type: TYPE_WEB,
+        image: "am-play-web.png"
+    },
+    {
+        key: "time-bridge",
+        name: "Time Bridge",
+        description: "Time Bridge is a Vue.js app that calculates time differences with live updates, preset bookmarks, and a responsive, animated Tailwind CSS-based interface for seamless user interaction.",
+        duration: "Vue.js",
+        repo: "https://github.com/mdalmamunDev/TimeBridge",
+        link: "https://mdalmamundev.github.io/TimeBridge",
+        type: TYPE_WEB,
+        image: "time-bridge.png"
+    },
+    {
+        key: "tisi-aircraft",
+        name: "TISI AirCraft",
+        description: "Aircraft game by me and team.",
+        duration: "Android Studio + Java",
+        repo: "https://github.com/mdalmamunDev/TISI-AirCraft",
+        link: "android-apps/TISI AirCraft.apk",
+        type: TYPE_ANDROID,
+        image: "tisi-aircraft.jpg"
+    },
+    {
+        key: "contact",
+        name: "Contact",
+        description: "Full functional Contact Android App(With All Basics: Add, Update, Delete, Making Call, SMS, Mail etc.)And of course with 👉Auto View Generator 🙂",
+        duration: "Android Studio + Java",
+        repo: "https://github.com/mdalmamunDev/contacts",
+        link: "android-apps/Contact.apk",
+        type: TYPE_ANDROID,
+        image: "contact.jpg"
+    },
+    {
+        key: "fun-tan",
+        name: "FunTan",
+        description: "Product management system with modern UI.",
+        duration: "Laravel + Vue.js",
+        repo: "https://github.com/mdalmamunDev/fun-tan",
+        link: "",
+        type: TYPE_WEB,
+        image: "fun-tan.png"
+    },
+    {
+        key: "fashion-bazaar",
+        name: "Fashion Bazaar",
+        description: "E-commerce platform featuring user authentication and product management.",
+        duration: "Laravel, Vue.js, MySQL",
+        repo: "https://github.com/mdalmamunDev/Fashion_Bazaar",
+        link: "",
+        type: TYPE_WEB,
+        image: "fashion-bazaar.png"
+    },
+    {
+        key: "unit-converter",
+        name: "Unit-Converter",
+        description: "Convert anything anytime 🙂.",
+        duration: "Android Studio + Java",
+        repo: "https://github.com/mdalmamunDev/Unit-Converter",
+        link: "android-apps/UnitConverter v3.x.apk",
+        type: TYPE_ANDROID,
+        image: ""
+    },
+    {
+        key: "dream-dwell",
+        name: "DreamDwell",
+        description: "Hotel management app for managing bookings and services.",
+        duration: "MERN Stack",
+        repo: "https://github.com/mdalmamunDev/DreamDwell",
+        link: "",
+        type: TYPE_WEB,
+        image: "dream-dwell.png"
+    },
+    {
+        key: "programmers-villa",
+        name: "ProgrammersVilla",
+        description: "Platform for hiring and viewing programmers' details.",
+        duration: "MERN Stack",
+        repo: "https://github.com/mdalmamunDev/ProgrammersVilla",
+        link: "",
+        type: TYPE_WEB,
+        image: "programmers-villa.png"
+    }
+];
